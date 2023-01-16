16a52e919d308f75dc3f0f2ac1c46b8b837efd76
DarioVarto <dariovartolo@hotmail.com>
2023-01-15 21:45:32 -0300

    Décimo commit "carga y actualización"

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 "use strict"
 
-let montoDisponible=Number(document.getElementById('montoDisponible').value) //Ingresos
+let montoDisponible=document.getElementById('montoDisponible') //Ingresos
 
 let ing=document.getElementById('ing') //Muestra de ingreso no modificable
 
-let misGastos=Number(document.getElementById('misGastos'))//Suma de los gastos realizados
+let misGastos=document.getElementById('misGastos')//Suma de los gastos realizados
 
-let misMontos=Number(document.getElementById('misMontos'))//Dinero disponible
+let misMontos=document.getElementById('misMontos')//Dinero disponible
 
 let btnIngresos=document.getElementById('btnIngresos')//Boton ingresos
 
@@ -16,6 +16,11 @@ let monto=document.getElementById('monto')//monto
 
 let listaGastos=document.getElementById('listaGastos')//ul gastos
 
+let btnGastosMq=document.getElementById('btnGastosMq')
+
+let gastoActualizado=0
+
+let montoActualizado=0
 
 
 
@@ -30,11 +35,33 @@ const ingreso = () => {
                          let misMontos=document.getElementById('misMontos')
    
                          misMontos.innerHTML=`$${montoDisponible}`
+                    
+                         misGastos.innerHTML='$'+Number(0)
+
+                         montoActualizado=Number(montoDisponible)
+                         
+                         
+                        return montoActualizado
+                         
+                        
 
 }
 
+//Función para anular nueva vuelta de ingresos
+
+const dsb = () => {montoDisponible.setAttribute("disabled", true)
+montoDisponible.setAttribute("placeholder", "")
+
+montoDisponible.value=""}
+
+
 
 btnIngresos.addEventListener('click',ingreso)
+
+btnIngresos.addEventListener('click',dsb)
+
+/* btnIngresos.addEventListener('click',dsb) */
+
 
 
 let btnGastos=document.getElementById('btnGastos')
@@ -42,7 +69,7 @@ let btnGastos=document.getElementById('btnGastos')
 
 const ingresoGastos = () => { 
 
-                            let concepto=document.getElementById('concepto').value 
+let concepto=document.getElementById('concepto').value 
 
 let monto=Number(document.getElementById('monto').value)
 
@@ -51,8 +78,20 @@ let listaGastos=document.getElementById('listaGastos')
 listaGastos.innerHTML+= `<li>${concepto} ${monto} <i class="bi bi-trash3-fill" id="eliminarGasto"></i></li>`
 
 
+
+gastoActualizado= Number(gastoActualizado)+Number(monto)
+
+
+misGastos.innerHTML=`$${gastoActualizado}`
+
+montoActualizado=Number(montoActualizado)-Number(monto)
+
+misMontos.innerHTML=montoActualizado
+
+
 }
 
 
 btnGastos.addEventListener('click',ingresoGastos)
 
+btnGastosMq.addEventListener('click',ingresoGastos)//Botón de Media Queries
